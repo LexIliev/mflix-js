11a048e5a538c42a6d4e6151ee36f1b69bba8f0b
Alex Iliev <aalex.iliev@gmail.com>
2019-06-30 15:09:14 +0300

    Add Ticket 10 - Create - Update Comments

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -15,20 +15,7 @@ export default class CommentsDAO {
   }
 
   /**
-  Ticket: Create/Update Comments
-
-  For this ticket, you will need to implement the following two methods:
-
-  - addComment
-  - updateComment
-
-  You can find these methods below this docstring. Make sure to read the comments
-  to better understand the task.
-  */
-
-  /**
    * Inserts a comment into the `comments` collection, with the following fields:
-
      - "name", the name of the user posting the comment
      - "email", the email of the user posting the comment
      - "movie_id", the _id of the movie pertaining to the comment
@@ -43,9 +30,14 @@ export default class CommentsDAO {
    */
   static async addComment(movieId, user, comment, date) {
     try {
-      // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      const commentDoc = {
+        movie_id: ObjectId(movieId),
+        name: user.name,
+        email: user.email,
+        text: comment,
+        date: date,
+      }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -66,12 +58,19 @@ export default class CommentsDAO {
    */
   static async updateComment(commentId, userEmail, text, date) {
     try {
-      // TODO Ticket: Create/Update Comments
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        {
+          _id: ObjectId(commentId),
+          email: userEmail,
+        },
+        {
+          $set: {
+            text,
+            date,
+          },
+        },
       )
 
       return updateResponse
