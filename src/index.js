6733bc638d32cbaf5adac2f95d05412594cc2eda
Alex Iliev <aalex.iliev@gmail.com>
2019-07-07 17:32:14 +0300

    Add Ticket 14 - Connection Pooling

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ import CommentsDAO from "./dao/commentsDAO"
 const port = process.env.PORT || 8000
 
 /**
-Ticket: Connection Pooling
-
-Please change the configuration of the MongoClient object by setting the
-maximum connection pool size to 50 active connections.
-*/
-
-/**
 Ticket: Timeouts
 
 Please prevent the program from waiting indefinitely by setting the write
@@ -22,11 +15,12 @@ concern timeout limit to 2500 milliseconds.
 
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
-  // Set the poolSize to 50 connections.
   // TODO: Timeouts
   // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
+  {
+    poolSize: 50,
+    useNewUrlParser: true,
+  },
 )
   .catch(err => {
     console.error(err.stack)
